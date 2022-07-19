dfb299adaeaefbc612613d44d66944f623d03409
kalz2q <katsu95i@gmail.com>
2022-07-19 19:26:21 +0900

    m

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import './App.css';
 const App = () => {
   return (
     <div className="App">
-      <img src="tenshi.JPG" alt="太郎のアイコン画像です"/>
+      <img src={require("./tenshi.JPG")} alt="太郎のアイコン画像です" />
       <h1>山田太郎</h1>
       <p>UI/UXデザイナー見習いです。日々の勉強、頑張っています。</p>
     </div>
