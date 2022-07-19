925d59bc581d73d36cf50e05ca3be9e5b5efe91c
kalz2q <katsu95i@gmail.com>
2022-07-20 01:59:38 +0900

    m

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,13 @@ import './App.css';
 const App = () => {
   return (
     <div className="App">
-      <img src={require("./tenshi.JPG")} alt="太郎のアイコン画像です" />
-      <h1>山田太郎</h1>
-      <p>UI/UXデザイナー見習いです。日々の勉強、頑張っています。</p>
+      <div className='section'>
+        <img src={require("./tenshi.JPG")} alt="太郎のアイコン画像です" />
+        <h1>山田太郎</h1>
+        <p>UI/UXデザイナー見習いです。日々の勉強、頑張っています。</p>
+      </div>
+
+      <footer>(c) dotinstallsample </footer>
     </div>
   );
 }
